7e1cbc1e0042d1ac47bbf8dd5681067c8a854a8e
mohanSrg <srgmohan7@gmail.com>
2016-02-23 19:49:38 +0530

    Instantiate script

---
+++ Assets/GameObjectScripts/InstantiateScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    GameObject.Instantiate(Resources.Load("Clone"), gameObject.transform.position,this.transform.rotation);
+	}
+}
