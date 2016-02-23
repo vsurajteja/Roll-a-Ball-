844fc40eab4b622739f8b99fb4e0e13fbbcff491
vsurajteja <vsurajteja@gmail.com>
2016-02-23 19:17:24 +0530

    Changed the rotation angles of the cubes

--- Assets/Scripts/Rotate1.cs
+++ Assets/Scripts/Rotate1.cs
@@ -7,7 +7,7 @@ public class Rotate1 : MonoBehaviour {
 	//This method rotates the cubes
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.Rotate (new Vector3 (30, 15, 45) * Time.deltaTime);
 	}
 
     public void UselessMethod()
